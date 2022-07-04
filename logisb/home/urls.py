86b9451d0c53299e72dce802b322cdb7367c6b53
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-07-04 11:53:40 +0530

    Initial code commit

---
+++ logisb/home/urls.py
@@ -0,0 +1,11 @@
+from xml.etree.ElementInclude import include
+from django import views
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.index , name ="home"),
+    path('login', views.loginuser, name ="login"),
+    path('logout', views.logoutuser, name ="logout"),
+]
